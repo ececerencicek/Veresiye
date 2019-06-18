3eefa1ea389b81bafc4151dfd562a7cb5b94fb2d
ccrncck@gmail.com <ccrncck@gmail.com>
2019-06-18 13:51:38 +0300

    first version

---
+++ Veresiye.Data/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Veresiye.Model;
+
+namespace Veresiye.Data
+{
+    //Repository context'e bağımlıdır .Contextteki Applicationdb ye bağımlıdır. 
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext db;
+        private readonly DbSet<T> entities;
+        public Repository(ApplicationDbContext context)
+        {
+            this.db = context;
+            this.entities = context.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            entities.Remove(entity);
+        }
+
+        public T Get(int id)
+        {
+            return entities.FirstOrDefault(x => x.Id == id);
+        }
+
+        public T Get(Expression<Func<T, bool>> where)
+        {
+            return entities.Where(where).FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.ToList();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> where)
+        {
+            return entities.Where(where).ToList();
+        }
+
+        public void Update(T entity)
+        {
+            entity.UpdatedAt = DateTime.Now;
+            entity.UpdatedBy = "unknown";
+            db.Entry<T>(entity).State = EntityState.Modified;
+        }
+
+        public void Insert(T entitiy)
+        {
+            entitiy.CreatedAt = DateTime.Now;
+            entitiy.CreateBy = "unknown";
+            entitiy.UpdatedAt = DateTime.Now;
+            entitiy.UpdatedBy = "unknown";
+            entities.Add(entitiy);
+        }
+    }
+}
---
+++ Veresiye.Model/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veresiye.Model
+{
+    public class BaseEntity
+    {
+        //ortak alanları buraya koyduk (Entitylerin ortak alanlarını)
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
